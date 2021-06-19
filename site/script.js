ac867e4668eb2b1043360385c9e576b992894aa9
arn25-lk <arnavb25@gmail.com>
2021-06-19 11:48:42 +0200

    constructs

--- site/script.js
+++ site/script.js
@@ -1,1 +1,61 @@
 var x = "Hello World";
+
+var message = "in global";
+console.log("global: message = " + message);
+
+var a = function(){
+    var message = "_inside a_";
+    console.log("a: message = " + message);
+    function c(){
+        console.log("c: message = " + message);
+    }
+    c();
+}
+//Outer reference is global
+function b(){
+    console.log("b: message = " + message);
+}
+
+var x = "4";
+var y = 4
+if(x == y){
+    console.log("x is equal to y");
+}
+if(x === y){
+    console.log("x is strictly equal to y");
+
+}else{
+    console.log("x is not strictly equal to y");
+}
+
+if(false || null || undefined || "" || 0 || NaN){
+    console.log("this line won't execute");
+}else{
+    console.log("All false");
+
+if(true && "hello" && 1 && -1 && "false"){
+    console.log("All true");
+}
+
+}
+function orderChickenWith(sideDish){
+    sideDish = sideDish || "whatever!";
+    console.log("Chicken with " + sideDish);
+}
+orderChickenWith();
+orderChickenWith("noodles");
+/* In JS and object is a collection of name/value pairs 
+   Primitve type represents a single, immutable value:
+   Symbol,
+   Undefined, 
+   Null, 
+   Number, 
+   String,
+   Boolean
+   _________
+   Object
+   */
+
+
+a();
+b();
